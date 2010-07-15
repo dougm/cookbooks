88fa57f4ef1f15ea24e8a13aa8704a18ff4ab740
Doug MacEachern <dougm@vmware.com>
2010-07-15 16:34:04 -0700

    add support for modifying HKEY_USERS data via ntuser.dat

--- windows/providers/registry.rb
+++ windows/providers/registry.rb
@@ -18,8 +18,27 @@
 # limitations under the License.
 #
 
+#if user profile (NTUSER.DAT) is in use, the switch to the active profile
+#by converting username to sid
+def username_to_sid(name, key_name)
+  begin
+    sid = WMI::Win32_UserAccount.find(:first, :conditions => {:name => name}).sid
+    path = key_name.split("\\")
+    if path[0] == name
+      path[0] = sid
+      key_name = path.join("\\")
+      Chef::Log.debug("HKEY_USERS: #{name} -> #{sid}")
+    end
+  rescue
+  end
+  key_name
+end
+
 def registry_update(mode)
   require 'win32/registry'
+  require 'ruby-wmi'
+
+  key_name = @new_resource.key_name
 
   hive = {
     :LOCAL_MACHINE => Win32::Registry::HKEY_LOCAL_MACHINE,
@@ -27,7 +46,18 @@ def registry_update(mode)
     :CURRENT_USER => Win32::Registry::HKEY_CURRENT_USER
   }[@new_resource.hkey || :LOCAL_MACHINE]
 
-  hive.send(mode, @new_resource.key_name, Win32::Registry::KEY_ALL_ACCESS) do |reg|
+  if @new_resource.load_key
+    @priv = Chef::WindowsPrivileged.new
+    @new_resource.load_key.each do |name,file|
+      Chef::Log.debug("RegLoadKey(#{name}, #{file})")
+      unless @priv.reg_load_key(name, file)
+        Chef::Log.debug("#{file} is in use (#{name} has active session?)")
+        key_name = username_to_sid(name, key_name) if hive == Win32::Registry::HKEY_USERS
+      end
+    end
+  end
+
+  hive.send(mode, key_name, Win32::Registry::KEY_ALL_ACCESS) do |reg|
     @new_resource.values.each do |k,val|
       key = "#{k}" #wtf. avoid "can't modify frozen string" in win32/registry.rb
       cur_val = nil
@@ -40,6 +70,15 @@ def registry_update(mode)
         Chef::Log.debug("#{@new_resource}: setting #{key}=#{val}")
         reg[key] = val
         @new_resource.updated = true
+      end
+    end
+  end
+
+  if @new_resource.load_key
+    @new_resource.load_key.keys.each do |name|
+      begin
+        @priv.reg_unload_key(name)
+      rescue
       end
     end
   end
